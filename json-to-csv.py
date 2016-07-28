548b11057b2577f016eb175e3a672da2072ceea5
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2016-07-28 11:23:14 -0500

    Key and list search
    Implemented a bunch of stuff to retrieve files and start working
    through them. I’m down to working with the data for each individual
    trial

--- json-to-csv.py
+++ json-to-csv.py
@@ -1,3 +1,55 @@
 import json
 import csv
-import os
+from os import listdir
+from os.path import isfile, join
+import Tkinter as tk
+
+
+
+def json_to_csv():
+    directory = "json/"
+
+    files = [f for f in listdir(directory) if isfile(join(directory, f))]
+    filepaths = []
+    data = []
+    for f in files:
+        if f[-5:] == ".json":
+            filepaths += [directory + f]
+        else:
+            print "File " + f + " isn't a json file! It won't be included."
+
+    for f in filepaths:
+        jsonfile = open(f)
+        data += [json.load(jsonfile)]
+
+    for d in data:
+        d_keys = d.keys()
+        headers = []
+        for x in xrange(0,len(d[d_keys[0]])):
+            headers += [d[d_keys[0]][x].keys()]
+
+        headers = [item for sublist in headers for item in sublist]
+        headers = list(set(headers))
+
+        csvfile = open('aggregate_data.csv', 'wb')
+        csv.DictWriter(csvfile, headers)
+
+        for key in d_keys:
+            #for each subject
+            curr_data = d[key]
+            for trial in curr_data:
+                rowDict = {}
+                print trial
+                return
+
+
+
+        csvfile.close()
+        # for key in d.keys():
+        #     print d[key].keys
+
+
+
+
+if __name__ == "__main__":
+    json_to_csv()
